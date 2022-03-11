e3a4bfa6f2136dd5f798b81f156d4db0a7bd13d9
devvspaces <netrobepy@gmail.com>
2022-03-11 08:38:55 +0100

    update

--- monitor/monitor/wsgi/base.py
+++ monitor/monitor/wsgi/base.py
@@ -3,5 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monitor.settings.production')
+os.environ.setdefault('ENVIRONMENT_VARIABLE', 'monitor.settings.production')
 
 application = get_wsgi_application()
