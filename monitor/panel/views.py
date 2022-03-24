dba8d15a5d58a6b6fa39872a909a60bcf5a7fd1f
devvspaces <netrobepy@gmail.com>
2022-03-24 11:41:06 +0100

    added the website status checking

--- monitor/panel/views.py
+++ monitor/panel/views.py
@@ -10,7 +10,7 @@ from django.views.generic import FormView, DetailView, TemplateView
 
 from utils.general import verify_next_link
 
-from services.models import Service
+from services.models import Service, Website
 
 from .forms import LoginForm
 
@@ -75,6 +75,9 @@ class Dashboard(LoginRequiredMixin, TemplateView):
 
         # Get available services
         context["services"] = Service.objects.all()
+
+        # Get websites
+        context['websites'] = Website.objects.all()
         return context
 
 
@@ -96,6 +99,24 @@ class ServiceLog(LoginRequiredMixin, DetailView):
         return context
 
 
+class WebsiteLog(LoginRequiredMixin, DetailView):
+    template_name = 'panel/website_tab.html'
+    model = Website
+    slug_field = 'conf_filename'
+    slug_url_kwarg = 'conf_filename'
+    context_object_name = 'website'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        website = self.get_object()
+
+        # Get service name
+        context["title"] = website.name
+
+        return context
+
+
 def recheck_service_status(request, service_name):
     obj = get_object_or_404(Service, service_name=service_name)
 
@@ -113,6 +134,35 @@ def get_logs_view(request):
         service_name = request.POST.get('service_name')
 
         obj = get_object_or_404(Service, service_name=service_name)
+
+        # Get the service logs
+        data = {
+            'log': obj.get_logs()
+        }
+    
+        return JsonResponse(data=data, status=200)
+    
+    messages.warning(request, 'Page does not exist')
+    return redirect('panel:dashboard')
+
+
+
+def recheck_website_status(request, conf_filename):
+    obj = get_object_or_404(Website, conf_filename=conf_filename)
+
+    # Update status
+    obj.recheck()
+
+    messages.success(request, f"{obj.name} website status is successfully reloaded")
+
+    return redirect(obj.get_absolute_url())
+
+def get_websites_logs_view(request):
+
+    if request.POST:
+        conf_filename = request.POST.get('conf_filename')
+
+        obj = get_object_or_404(Website, conf_filename=conf_filename)
 
         # Get the service logs
         data = {
--- monitor/services/models.py
+++ monitor/services/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
-from utils.general import get_version, get_active_state, get_service_logs
+from utils.general import get_version, get_active_state, get_service_logs, get_website_state, get_website_logs
 
 class Service(models.Model):
     name = models.CharField(max_length=40)
@@ -33,3 +33,47 @@ class Service(models.Model):
     
     def get_absolute_url(self):
         return reverse('panel:service', args=[self.service_name])
+
+
+class Website(models.Model):
+    name = models.CharField(max_length=40)
+
+    # These data are without their extension
+    conf_filename = models.CharField(max_length=100, unique=True)
+    log_filename = models.CharField(max_length=100)
+
+    website_link = models.URLField(unique=True)
+    active = models.BooleanField(default=True)
+    last_checked = models.DateTimeField(null=True, blank=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    # Get the location of the website conf file
+    def get_conf_full_path(self):
+        return f"/etc/apache2/sites-enabled/{self.conf_filename}.conf"
+    
+    # Get the full path to the log of the website
+    def get_log_full_path(self):
+        return f"/var/log/apache2/{self.log_filename}"
+
+    # Recheck the status of the website
+    def recheck(self):
+        status = get_website_state('http://'+self.website_link)
+        if status:
+            self.active = True
+        else:
+            self.active = False
+        self.last_checked = timezone.now()
+        self.save()
+    
+    # Get the logs of the website
+    def get_logs(self):
+        return get_website_logs(self.get_log_full_path())
+    
+    # Get the status of the website in text
+    def get_last_status(self) -> str:
+        return 'active' if self.active else 'inactive'
+    
+    def get_absolute_url(self):
+        return reverse('panel:website', args=[self.conf_filename])
